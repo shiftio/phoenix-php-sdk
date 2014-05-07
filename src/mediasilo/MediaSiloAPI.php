9d03d3aa6b9cb39c7b6ee593b5c31d5d42ab6e95
Tori Holmes-Kirk <tori.kirk@gmail.com>
2014-05-07 11:01:24 -0400

    Namespacing bug for exception

--- src/mediasilo/MediaSiloAPI.php
+++ src/mediasilo/MediaSiloAPI.php
@@ -3,11 +3,11 @@
 namespace mediasilo;
 
 if (!function_exists('curl_init')) {
-    throw new Exception('We need cURL for the API to work. Have a look here: http://us3.php.net/curl');
+    throw new \Exception('We need cURL for the API to work. Have a look here: http://us3.php.net/curl');
 }
 
 if (!function_exists('json_decode')) {
-    throw new Exception('We need json_decode for the API to work. If you\'re running a linux distro install this package: php-pecl-json');
+    throw new \Exception('We need json_decode for the API to work. If you\'re running a linux distro install this package: php-pecl-json');
 }
 
 use mediasilo\http\MediaSiloResourcePaths;
